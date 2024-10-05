4e8f50bea37f78d95526f82f5730853d8782fbe2
Samin Zaman <samin.zaman325@gmail.com>
2024-10-05 09:10:25 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -13,3 +13,7 @@ sudo rm -f libcudnn*
 cd /usr/local/cuda-12.1/include
 sudo rm -f cudnn*
 cd ~/TechtonicActivity/
+cd space_apps_2024_seismic_detection/data/lunar/training/data/S12_GradeA
+mv xa.s12.00.mhz.1971-04-13HR02_evid00029.mseed xa.s12.00.mhz.1971-04-13HR00_evid00029.mseed
+cd ~/TechtonicActivity/
+source env_test/bin/activate
