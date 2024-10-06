c4e6a1180fe8fed5f15b2b47abbfb59f5f3026da
Samin Zaman <samin.zaman325@gmail.com>
2024-10-06 10:52:22 -0400

    SeismicDataset.py for mars

--- SeismicDataset.py
+++ SeismicDataset.py
@@ -27,6 +27,10 @@ class SeismicDataset(Dataset):
         
         # Load the miniseed file using obspy
         file_path = os.path.join(self.data_dir, file_name)
+        
+        # IMPORTANT: IF USING THE MARS DATASET, UNCOMMENT THE FOLLOWING 1 LINE OF CODE
+        # file_path = file_path[:-4]
+        
         file_path = file_path + '.mseed'
         st = obspy.read(file_path)
         data = st[0].data  # Assumes one trace per file
