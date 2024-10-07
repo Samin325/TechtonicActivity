969a3b251a2cddf5179ed3c529815cad36b75598
Aakash1417 <durairaj@ualberta.ca>
2024-10-06 20:36:55 -0600

    added lunar catalogue file

--- sta-lta/sta_lta_seismic_analysis.py
+++ sta-lta/sta_lta_seismic_analysis.py
@@ -1,26 +1,22 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from obspy import read
-from scipy.signal import spectrogram, welch
-from obspy.signal.invsim import cosine_taper
-from obspy.signal.filter import highpass
-from obspy.signal.trigger import classic_sta_lta, plot_trigger, trigger_onset, recursive_sta_lta
 import os
-import zipfile
 import glob
-import random
-import bisect
 from skopt import gp_minimize
 from skopt.space import Real
+import pickle
+import os
 
-from helpers import implement_STA_LTA, score_function, show_sound_plot, calculate_noise_level, split_files_into_buckets
+from helpers import implement_STA_LTA, score_function, show_sound_plot, split_files_into_buckets
 
 lunar_training_folder_path = './space_apps_2024_seismic_detection/data/lunar/training/data/S12_GradeA'
-lunar_training_mseed_files = glob.glob(os.path.join(lunar_training_folder_path, '*.mseed'))
+lunar_training_mseed_files = glob.glob(
+    os.path.join(lunar_training_folder_path, '*.mseed'))
 
 lunar_test_folder_path = './space_apps_2024_seismic_detection/data/lunar/test/data/S16_GradeA'
-lunar_test_mseed_files = glob.glob(os.path.join(lunar_test_folder_path, '*.mseed'))
+lunar_test_mseed_files = glob.glob(
+    os.path.join(lunar_test_folder_path, '*.mseed'))
 
 lunar_catalog_csv_file = './space_apps_2024_seismic_detection/data/lunar/training/catalogs/apollo12_catalog_GradeA_final.csv'
 
@@ -31,14 +27,17 @@ def objective_function(params):
     total_acc = 0
 
     for quak_file in bucket_data:
-        detected_triggers = implement_STA_LTA(quak_file['trace'], sta_len, lta_len, thr_on, thr_off)
+        detected_triggers = implement_STA_LTA(
+            quak_file['trace'], sta_len, lta_len, thr_on, thr_off)
 
         # show_sound_plot(quak_file['trace'], detected_triggers)
-        f1, accuracy = score_function(lunar_true_triggers.loc[lunar_true_triggers['filename'] == quak_file['filename']]['time_rel(sec)'], detected_triggers)
+        f1, accuracy = score_function(
+            lunar_true_triggers.loc[lunar_true_triggers['filename'] == quak_file['filename']]['time_rel(sec)'], detected_triggers)
         total_f1 += f1
         total_acc += accuracy
 
     return -total_f1
+
 
 def find_f1(params, dats, showplot=False):
     sta_len, lta_len, thr_on, thr_off = params
@@ -46,31 +45,30 @@ def find_f1(params, dats, showplot=False):
     total_acc = 0
 
     for quak_file in dats:
-        detected_triggers = implement_STA_LTA(quak_file['trace'], sta_len, lta_len, thr_on, thr_off)
+        detected_triggers = implement_STA_LTA(
+            quak_file['trace'], sta_len, lta_len, thr_on, thr_off)
 
         if showplot:
             show_sound_plot(quak_file['trace'], detected_triggers)
-        f1, accuracy = score_function(lunar_true_triggers.loc[lunar_true_triggers['filename'] == quak_file['filename']]['time_rel(sec)'], detected_triggers)
+        f1, accuracy = score_function(
+            lunar_true_triggers.loc[lunar_true_triggers['filename'] == quak_file['filename']]['time_rel(sec)'], detected_triggers)
         total_f1 += f1
         total_acc += accuracy
 
     return -total_f1
 
-def categorize(trace):
-    noise = calculate_noise_level(trace.data)
-    index = bisect.bisect(lunar_training_buckets_max, noise)
-    return index
 
 if __name__ == '__main__':
     all_lunar_training_data = []
     for file_path in lunar_training_mseed_files:
-        filename = file_path.split('/')[-1].removesuffix('.mseed')
+        filename = os.path.splitext(file_path)[0]
         st = read(file_path)
         tr = st[0]
         all_lunar_training_data.append({'trace': tr, 'filename': filename})
     lunar_true_triggers = pd.read_csv(lunar_catalog_csv_file)
 
-    buckets, lunar_training_buckets_max = split_files_into_buckets(all_lunar_training_data, num_buckets=5)
+    buckets, lunar_training_buckets_max = split_files_into_buckets(
+        all_lunar_training_data, num_buckets=5)
     print(f'Bucket boundaries: {lunar_training_buckets_max}')
 
     search_space = [
@@ -79,22 +77,20 @@ if __name__ == '__main__':
         Real(2, 4, name='thr_on'),
         Real(0.5, 2.0, name='thr_off')
     ]
-    best_params_for_bucket={}
+    best_params_for_bucket = {}
     for bucket_num, buck in buckets.items():
         bucket_data = buck
 
-        result = gp_minimize(objective_function, search_space, n_calls=100)
+        result = gp_minimize(objective_function, search_space, n_calls=70)
         print(f"Bucket {bucket_num}:")
         best_params = result.x
         best_params_for_bucket[bucket_num] = best_params
         print(f"Best parameters for Bayse optimizer: {best_params}")
         print(f"Average f1: {-objective_function(best_params)/len(buck)}")
-        print()    
+        print()
 
-    for quak_file in all_lunar_training_data:
-        index = categorize(quak_file['trace'])
-        params = best_params_for_bucket[index + 1]
-        print(quak_file['filename'])
-        f1 = find_f1(params, [quak_file], True)
-        print(index)
+        with open('best_params_for_bucket_lunar.pkl', 'wb') as f:
+            pickle.dump(best_params_for_bucket, f)
 
+        with open('lunar_training_buckets_max.pkl', 'wb') as f:
+            pickle.dump(lunar_training_buckets_max, f)
